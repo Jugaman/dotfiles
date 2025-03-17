57591ef10fb10d6712c50a7d017e6b4b96cdeaae
Jugaman <jugamanSWE@proton.me>
2025-03-16 22:04:13 -0300

    phantom gloom finished

--- .config/wezterm/chromatic/chromatic-palette.lua
+++ .config/wezterm/chromatic/chromatic-palette.lua
@@ -7,22 +7,22 @@ local phantom_gloom = {
 	sonic_silver = "#7A7A7A",
 	pastel_green = "#7EED6F",
 	iguana_green = "#7ABA78",
-	dark_spring_green = "#7ABA78",
+	islamic_green = "#059212",
 	jasper = "#D84040",
-	deep_dumpling = "#982B1C",
+	ruby_red = "#FF1E1E",
 	tulip = "#F18196",
+	dark_orange = "#FF8F00",
 	dark_salmon = "#F19A81",
 	smoked_latte = "#C9AF94",
 	maize = "#F9CB43",
 	key_lime = "#E9F28E",
-	navajo_white = "#FFE4A7",
-	dark_purple = "#311131",
+	dark_orchid = "#A31ACB",
 	wisteria = "#8967B3",
-	dark_cerulean = "#756AB6",
-	bright_navy_blue = "#117AE0",
+	egyptian_blue = "#1230AE",
+	brandeis_blue = "#117AE0",
 	java = "#259797",
 	sky_blue = "#64CCDA",
-	ceil = "#90AACB",
+	aquamarine = "#6FEDD6",
 	platinum = "#E4E4E3",
 	smokey_white = "#F5F5F5",
 }
@@ -33,29 +33,29 @@ local chromatic_scheme = {
 	foreground = phantom_gloom.smokey_white,
 	cursor_bg = phantom_gloom.pastel_green,
 	cursor_fg = phantom_gloom.night_rider,
-	selection_bg = phantom_gloom.navajo_white,
+	selection_bg = phantom_gloom.key_lime,
 	selection_fg = phantom_gloom.night_rider,
-	split = phantom_gloom.dark_spring_green,
+	split = phantom_gloom.pastel_green,
 
 	ansi = {
 		"#0C0C0C", -- black (vampire_black)
-		"#982B1C", -- red (deep_dumpling)
-		"#176843", -- green (iguana_green)
+		"#D84040", -- red (jasper)
+		"#059212", -- green (islamic_green)
 		"#F9CB43", -- yellow (key_lime)
-		"#094374", -- blue (ceil)
-		"#311131", -- magenta (cyber_grape)
-		"#259797", -- cyan (java)
+		"#1230AE", -- blue (egyptian_blue)
+		"#8967B3", -- magenta (wisteria)
+		"#64CCDA", -- cyan (sky_blue)
 		"#E4E4E3", -- white (smokey_white)
 	},
 
 	brights = {
 		"#272121", -- bright black (raisin_black)
-		"#D84040", -- bright red (jasper)
+		"#FF1E1E", -- bright red (ruby_red)
 		"#7EED6F", -- bright green (pastel_green)
 		"#E9F28E", -- bright yellow (maize)
-		"#117AE0", -- bright blue (bright_navy_blue)
-		"#8967B3", -- bright magenta (wisteria)
-		"#64CCDA", -- bright cyan (sky_blue)
+		"#117AE0", -- bright blue (brandeis_blue)
+		"#A31ACB", -- bright magenta (dark_orchid)
+		"#6FEDD6", -- bright cyan (aquamarine)
 		"#F5F5F5", -- bright white (platinum)
 	},
 
